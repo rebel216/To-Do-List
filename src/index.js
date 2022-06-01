5d97f97f8a65ae9dae259a0e929645a00bbd7a3f
rebel216 <neerajbhardwajpkt@gmail.com>
2022-06-01 21:43:31 +0530

    changes req in PR
    Added changes req in PR

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
 //import _ from 'lodash';
 import './style.css';
 
-const toDoItems = [{ 'index': 0, 'description': 'some description', 'completed': true }, { 'index': 1, 'description': 'some description', 'completed': false },
-{ 'index': 2, 'description': 'some description', 'completed': false },
-{ 'index': 3, 'description': 'some description', 'completed': true }];
+const toDoItems = [{ 'index': 0, 'description': 'some description1', 'completed': true }, { 'index': 1, 'description': 'some description', 'completed': false },
+{ 'index': 2, 'description': 'some description2', 'completed': false },
+{ 'index': 3, 'description': 'some description3', 'completed': true }];
 
-const toDoList = document.querySelectorAll('.todo');
+
 const forLable = document.querySelectorAll('.label');
 const chkMark = document.querySelectorAll('.fa-check');
 const chkBtn = document.querySelectorAll('.fa-square');
 
-const  Iterate = () => {
-    chkMark.forEach((chkMark) => {
-        chkMark.style.display = 'none';
-    });
-    chkBtn.forEach((chkBtn) => {
-        chkBtn.style.display = 'block';
-    });
+const Iterate = () => {
+    const toDoList = document.getElementById('todo');
+        
     for (let i = 0; i < toDoItems.length; i += 1) {
-        toDoList[i].innerHTML = toDoItems[i].description;
-        toDoList[i].setAttribute('data-hidden', toDoItems[i].completed);
-        toDoList[i].setAttribute('id', `${toDoItems[i].index}`);
-        forLable[i].setAttribute('for', `${toDoItems[i].index}`);
+        const item = document.createElement('li')
+        item.setAttribute('id', toDoItems[i].id);
+        item.innerHTML = toDoItems[i].description;
+        toDoList.appendChild(item);
     }
+   
 }
 
 Iterate();
