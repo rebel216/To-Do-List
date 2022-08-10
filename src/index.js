7196379bd83b3e8ebe9209c12c0baa82fd6c40dd
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-08-10 08:26:52 +0530

    Todo list sneak peak

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+//import _ from 'lodash';
 import './style.css';
-import { getStatus } from './List.js';
-import { displayStored } from './Add.js';
-import { editToDo, clearCompleted } from './Remove.js';
 
-function main() {
-    getStatus();
-    displayStored();
-    editToDo();
-    clearCompleted();
+const toDoItems = [{ 'index': 0, 'description': 'some description1', 'completed': true }, { 'index': 1, 'description': 'some description', 'completed': false },
+{ 'index': 2, 'description': 'some description2', 'completed': false },
+{ 'index': 3, 'description': 'some description3', 'completed': true }];
+
+
+const forLable = document.querySelectorAll('.label');
+const chkMark = document.querySelectorAll('.fa-check');
+const chkBtn = document.querySelectorAll('.fa-square');
+
+const Iterate = () => {
+    const toDoList = document.getElementById('todo');
+        
+    for (let i = 0; i < toDoItems.length; i += 1) {
+        const item = document.createElement('li')
+        item.setAttribute('id', toDoItems[i].id);
+        item.innerHTML = toDoItems[i].description;
+        toDoList.appendChild(item);
+    }
+   
 }
 
-main();
+Iterate();
